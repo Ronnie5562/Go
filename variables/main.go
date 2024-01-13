52fc05b0380cf451f11a7bf314ce665ffd6e3411
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2024-01-13 18:28:10 +0200

    Created: Created main.go

---
+++ variables/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+const LoginToken = "mombojumbogibberish"
+
+func main() {
+	fmt.Println("let")
+
+	fmt.Println(LoginToken)
+	fmt.Printf("Variable is of type: %T \n", LoginToken)
+}
