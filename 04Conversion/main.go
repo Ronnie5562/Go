ad448eb6fd2a1c9fadc6199aa3fff0a728c75faa
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2024-01-13 18:27:50 +0200

    Created: Created main.go

--- 04Conversion/main.go
+++ 04Conversion/main.go
@@ -9,17 +9,19 @@ import (
 )
 
 func main() {
-	fmt.Println("Welcome to our App")
+	fmt.Println("Welcome to our pizza app !!!")
 	fmt.Println("Please rate our pizza between 1 and 5")
 
 	reader := bufio.NewReader(os.Stdin)
-	input, _ := reader.ReadString('\n') // Since we don't intend to use the error varible, we can just represent t with a * _ *
 
-	rating, err := strconv.ParseFloat(strings.TrimSpace(input), 64)
+	input, _ := reader.ReadString('\n')
+	fmt.Println("Thanks for rating, ", input)
+
+	numRating, err := strconv.ParseFloat(strings.TrimSpace(input), 64)
 
 	if err != nil {
 		fmt.Println(err)
 	} else {
-		fmt.Println("Thanks for rating, ", rating + 1)
+		fmt.Printf("Added 1 to rating \n", numRating+1)
 	}
 }
