db66c9d88b9f184593e6953503e5c962652c041c
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2025-05-11 19:36:52 +0200

    Added the inspect command

---
+++ projects/pokedexcli/command_inspect.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func callbackInspect(cfg *config, args ...string) error {
+	if len(args) != 1 {
+		return errors.New("no pokemon name provided")
+	}
+
+	pokemonName := args[0]
+
+	pokemon, ok := cfg.caughtPokemon[pokemonName]
+	if !ok {
+		return fmt.Errorf("you haven't caught pokemon %s yet", pokemonName)
+	}
+
+	fmt.Printf("Name: %s\n", pokemon.Name)
+	fmt.Printf("Height: %d\n", pokemon.Height)
+	fmt.Printf("Weight: %d\n", pokemon.Weight)
+	fmt.Printf("Base Experience: %d\n", pokemon.BaseExperience)
+	fmt.Printf("Abilities: %v\n", pokemon.Abilities)
+	fmt.Printf("Stats:\n")
+	for _, stat := range pokemon.Stats {
+		fmt.Printf("  - %s: %d\n", stat.Stat.Name, stat.BaseStat)
+	}
+	fmt.Printf("Types:\n")
+	for _, t := range pokemon.Types {
+		fmt.Printf("  - %s\n", t.Type.Name)
+	}
+	fmt.Printf("Moves:\n")
+	for _, move := range pokemon.Moves {
+		fmt.Printf("  - %s\n", move.Move.Name)
+	}
+
+	return nil
+}
